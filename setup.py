546f74b4ff651e173c4e473faba46078440f0f64
ludeeus <joasoe@gmail.com>
2020-04-04 09:37:18 +0000

    Auto set version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="queueman",
-    version="0.3",
+    version="0.0",
     author="Joakim Sorensen",
     author_email="hi@ludeeus.dev",
     description="",
